cd966bd924cf96f7f8e1fa837e3c73fd1140c391
DESKTOP-2BG1LFQ\laopin <laopin2@gmail.com>
2019-06-26 10:21:05 +0300

    PB06262019_2. Project reinit.

--- Debug/sources.mk
+++ Debug/sources.mk
@@ -26,11 +26,12 @@ CPP_DEPS :=
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
+Components/ft5336 \
 Drivers/STM32F7xx_HAL_Driver/Src \
 Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Src \
 Middlewares/ST/STM32_USB_Host_Library/Core/Src \
+Middlewares/ST/TouchGFX/touchgfx/framework/include/platform/driver/touch \
 Middlewares/ST/TouchGFX/touchgfx/framework/source/platform/driver/touch \
-Middlewares/ST/TouchGFX/touchgfx/framework/source/platform/hal/simulator/sdl2 \
 Middlewares/ST/TouchGFX/touchgfx/framework/source/touchgfx/containers \
 Middlewares/ST/TouchGFX/touchgfx/framework/source/touchgfx/containers/clock \
 Middlewares/ST/TouchGFX/touchgfx/framework/source/touchgfx/containers/progress_indicators \
@@ -44,6 +45,32 @@ Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS \
 Middlewares/Third_Party/FreeRTOS/Source \
 Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1 \
 Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang \
+STM32746G-Discovery \
 Src \
 Startup \
+TouchGFX/build/MINGW32_NT-6.2/generated/fonts/src \
+TouchGFX/build/MINGW32_NT-6.2/generated/gui_generated/src/common \
+TouchGFX/build/MINGW32_NT-6.2/generated/gui_generated/src/screen1_screen \
+TouchGFX/build/MINGW32_NT-6.2/generated/gui_generated/src/screen2_screen \
+TouchGFX/build/MINGW32_NT-6.2/generated/images/src \
+TouchGFX/build/MINGW32_NT-6.2/generated/images/src/__designer \
+TouchGFX/build/MINGW32_NT-6.2/generated/texts/src \
+TouchGFX/build/MINGW32_NT-6.2/gui/src/common \
+TouchGFX/build/MINGW32_NT-6.2/gui/src/model \
+TouchGFX/build/MINGW32_NT-6.2/gui/src/screen1_screen \
+TouchGFX/build/MINGW32_NT-6.2/gui/src/screen2_screen \
+TouchGFX/build/MINGW32_NT-6.2/simulator \
+TouchGFX/build/MINGW32_NT-6.2/touchgfx/framework/source/platform/driver/touch \
+TouchGFX/build/MINGW32_NT-6.2/touchgfx/framework/source/platform/hal/simulator/sdl2 \
+TouchGFX/generated/fonts/src \
+TouchGFX/generated/gui_generated/src/common \
+TouchGFX/generated/gui_generated/src/screen1_screen \
+TouchGFX/generated/gui_generated/src/screen2_screen \
+TouchGFX/generated/images/src \
+TouchGFX/generated/images/src/__designer \
+TouchGFX/generated/texts/src \
+TouchGFX/gui/src/common \
+TouchGFX/gui/src/model \
+TouchGFX/gui/src/screen1_screen \
+TouchGFX/gui/src/screen2_screen \
 
